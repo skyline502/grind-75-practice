ea29b0269adf13b57cf68707c0ee022215303f02
skyline502 <johnny.san@gmail.com>
2022-11-23 13:28:36 -0800

    set up inverted bt, with one example for testing

--- week1/06-inverted-binary-tree.js
+++ week1/06-inverted-binary-tree.js
@@ -46,3 +46,19 @@ const invertTree = root => {
 };
 
 //Examples:
+const a = new Node(4);
+const b = new Node(2);
+const c = new Node(7);
+const d = new Node(1);
+const e = new Node(3);
+const f = new Node(6);
+const g = new Node(9);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.left = f;
+c.right = g;
+
+console.log(invertTree(a));
