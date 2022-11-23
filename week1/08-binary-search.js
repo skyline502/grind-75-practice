744fef736b18b975b54ec229a01d96a9c63d7af9
skyline502 <johnny.san@gmail.com>
2022-11-23 14:24:58 -0800

    passes most test cases

--- week1/08-binary-search.js
+++ week1/08-binary-search.js
@@ -19,6 +19,39 @@ Explanation: 2 does not exist in nums so return -
 */
 
 //your code here...
+//first check last number in nums, if target is > return -1;
+//start, end pointer
+//calculate mid point
+//compare nums[mid] to target and adjust start and end point accordingly
+//repeat until you find tar or start > end;
 const search = (nums, tar) => {
+  if (nums[nums.length - 1] < tar) {
+    return -1;
+  }
+  let start = 0;
+  let end = nums.length - 1;
 
+  while (start < end) {
+    let mid = Math.floor((start + end)/2);
+    // console.log(start, end)
+
+    if (nums[mid] === tar) return mid;
+    if (nums[start] === tar) return start;
+    if (nums[end] === tar) return end;
+
+    if (tar < nums[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return -1;
 };
+
+//Examples:
+let nums = [-1,0,3,5,9,12]
+console.log(search(nums, 9))//=> 4
+
+console.log(search(nums, 2)) // => -1
+console.log(search(nums, 13)) // => -1
