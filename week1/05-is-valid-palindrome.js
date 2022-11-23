5a6507d25d4a1ec487937d8e033f70ca15a567ee
skyline502 <johnny.san@gmail.com>
2022-11-23 10:56:05 -0800

    wrong approach because it asks to make all leters lowercase and REMOVE all punctuation, did not remove all punctuation so some cases failed

--- week1/05-is-valid-palindrome.js
+++ week1/05-is-valid-palindrome.js
@@ -34,13 +34,50 @@ Since an empty string reads the same forward and backward, it is a palindrome.
 //if different return false
 
 const isPalindrome = s => {
-  let converted = s.toLowerCase().split(' ').join('');
-  return converted;
+  let converted = s.toLowerCase().split(' ').join('').split('');
+  let alphanumeric = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  let start = 0;
+  let end = converted.length - 1;
+
+  while(start <= end) {
+    if (!alphanumeric.includes(converted[start])) {
+      console.log('does it reach here')
+      converted[start] = ' ';
+      console.log(converted);
+      start++;
+    }
+
+    if (!alphanumeric.includes(converted[end])) {
+      console.log('does it reach here')
+      converted[end] = ' ';
+      console.log(converted);
+      end--;
+    }
+    if (converted[start] !== converted[end]) {
+      console.log(`Start: ${converted[start]} End: ${converted[end]}`)
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
 };
 
 
 
 //Examples:
 let s = "A man, a plan, a canal: Panama";
+console.log(isPalindrome(s)) //true;
 
-console.log(isPalindrome(s))
+// s = "race a car";
+// console.log(isPalindrome(s)) //false;
+
+// s = " ";
+// console.log(isPalindrome(s)) //true;
+
+s = ",.";
+console.log(isPalindrome(s)); //true;
+
+s = "......a.....";
+console.log(isPalindrome(s)); //true;
