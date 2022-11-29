a861e8004eedc051548cac9d6d2ad6225cceeaad
skyline502 <johnny.san@gmail.com>
2022-11-29 13:57:44 -0800

    added examples, and ran through each example to see how the solution works

--- week1/11-balanced-bt.js
+++ week1/11-balanced-bt.js
@@ -3,6 +3,13 @@
 /*
 Given a binary tree, determine if it is height-balanced.
 
+what is height balanced???
+
+A height balanced binary tree is a binary tree in which the height of the left subtree and right subtree
+of any node does not differ by more than 1 and both the left and right subtree are also height balanced.
+
+so we have to return a number < 2, if number > 2 return false...
+
 
 
 Example 1:
@@ -46,7 +53,7 @@ The number of nodes in the tree is in the range [0, 5000].
 */
 
 const isBalanced = root => {
-  if (!root) {
+  if (!root) { //no node so the tree is balanced.
     return true;
   }
   let diff = Math.abs(height(root.left) - height(root.right));
@@ -80,3 +87,30 @@ c.left = d;
 c.right = e;
 
 console.log(isBalanced(a));
+
+// Example 1:
+
+//       3 //=> Math.abs(0 - 1) => 1 < 2 && isBalanced(9) && isBalanced(20)
+//     /   \
+//    9    20 //=> 1 + Math.max(0,0) => 1
+//  //=>0 /   \
+//      15    7
+      //=> 0  //=> 0
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: true
+
+
+// Example 2:
+
+//             1 //Math.abs(2 - 0) => 2 < 2 ? False
+//           /  \
+//          2    2 //=> 0
+//        /  \
+//       3    3 //1 + Math.max(1, 0) => 2
+//     /  \
+//    4    4 //=> 1 + Math.max(0, 0) => 1
+
+
+// Input: root = [1,2,2,3,3,null,null,4,4]
+// Output: false
