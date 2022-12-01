241b04e34729f4b17ad2f5bdcfde741f1e03c1ba
skyline502 <johnny.san@gmail.com>
2022-12-01 11:44:57 -0800

    fixed pairboarding answer

--- pairboarding/12-01-2022.js
+++ pairboarding/12-01-2022.js
@@ -13,48 +13,48 @@
 // We repeatedly make k duplicate removals on s until we no longer can.
 
 // Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
-'''
-You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them,
-causing the left and the right side of the deleted substring to concatenate together.
+// '''
+// You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them,
+// causing the left and the right side of the deleted substring to concatenate together.
 
-We repeatedly make k duplicate removals on s until we no longer can.
+// We repeatedly make k duplicate removals on s until we no longer can.
 
-Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
-'''
+// Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+// '''
 
-Example 1
-Input: s = "abcd", k = 2
-Output: "abcd"
-Explanation: There's nothing to delete.
+// Example 1
+// Input: s = "abcd", k = 2
+// Output: "abcd"
+// Explanation: There's nothing to delete.
 
-Example 2
-Input: s = "deeedbbcccbdaa", k = 3
-Output: "aa"
-Explanation:
-First delete "eee" and "ccc", get "ddbbbdaa"
-Then delete "bbb", get "dddaa"
-Finally delete "ddd", get "aa"
+// Example 2
+// Input: s = "deeedbbcccbdaa", k = 3
+// Output: "aa"
+// Explanation:
+// First delete "eee" and "ccc", get "ddbbbdaa"
+// Then delete "bbb", get "dddaa"
+// Finally delete "ddd", get "aa"
 
-Example 3:
-Input: s = "pbbcggttciiippooaais", k = 2
-Output: "ps"
+// Example 3:
+// Input: s = "pbbcggttciiippooaais", k = 2
+// Output: "ps"
 
-stack []
+// stack []
 
-for loop
-push current letter into stack [letter, 1], if same as previous increment
-if count of letter === k, pop it off the stack
-after loop
+// for loop
+// push current letter into stack [letter, 1], if same as previous increment
+// if count of letter === k, pop it off the stack
+// after loop
 
-iterate through stack, and concatenate to result string, how many letters to result based on count
-return result string
+// iterate through stack, and concatenate to result string, how many letters to result based on count
+// return result string
 
 const removeDupes = (s, k) => {
 stack = []
 
   for (let i = 0; i < s.length; i++) {
   	let letter = s[i];
-    if (stack[stack.length - 1][0] !== letter) {
+    if (stack.length === 0 || stack[stack.length - 1][0] !== letter) {
       stack.push([letter, 1])
     } else {
       stack[stack.length - 1][1] += 1;
@@ -67,9 +67,16 @@ stack = []
 
   let result = '';
   for (let i = 0; i < stack.length; i++) {
-    [letter, count] = [stack[0], stack[1]]
+    [letter, count] = [stack[i][0], stack[i][1]]
+    // console.log(letter, count)
     result += letter.repeat(count);
   }
 
   return result;
 }
+
+
+//Examples:
+let s = "pbbcggttciiippooaais";
+console.log(removeDupes(s, 2));
+// Output: "ps"
