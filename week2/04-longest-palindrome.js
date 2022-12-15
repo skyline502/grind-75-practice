e40ab1e452c0aff86fcfa48a24ef07e753fc8316
skyline502 <johnny.san@gmail.com>
2022-12-14 21:00:14 -0800

    approach is wrong, cant figure out what to do with odd count of letters

--- week2/04-longest-palindrome.js
+++ week2/04-longest-palindrome.js
@@ -9,7 +9,26 @@ Letters are case sensitive, for example, "Aa" is not considered a palindrome her
 
 //your code here...
 const longestPalindrome = s => {
-
+  let count = {}
+  for (let char of s) {
+    if (!count[char]) {
+      count[char] = 1;
+    } else {
+      count[char]++;
+    }
+  }
+  let longest = 0;
+  let single = 0;
+  for (let char in count) {
+    if (count[char] % 2 === 0) {
+      longest += count[char]
+    } else if (count[char] % 2 !== 0) {
+      if (count[char] > single) {
+        single = count[char];
+      }
+    }
+  }
+  return longest + single;
 }
 
 
@@ -22,3 +41,7 @@ console.log(longestPalindrome(s)) //'dccaccd' is the longest that can be built=>
 s = 'a'
 
 console.log(longestPalindrome(s)) //'a' => 1
+
+s = 'bananas';
+
+console.log(longestPalindrome(s)) // 'anana' => 5
