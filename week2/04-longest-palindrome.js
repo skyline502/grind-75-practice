9e0654aff610ff6439a8813f72e3e61162d32ee7
skyline502 <johnny.san@gmail.com>
2022-12-14 20:50:28 -0800

    added longest palindrome that can be built with given string

---
+++ week2/04-longest-palindrome.js
@@ -0,0 +1,24 @@
+//Longest Palindrome
+
+/*
+Given a string s which consists of lowercase or uppercase letters,
+return the length of the longest palindrome that can be built with those letters.
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+*/
+
+//your code here...
+const longestPalindrome = s => {
+
+}
+
+
+//Examples:
+
+let s = 'abccccdd';
+
+console.log(longestPalindrome(s)) //'dccaccd' is the longest that can be built=> 7
+
+s = 'a'
+
+console.log(longestPalindrome(s)) //'a' => 1
