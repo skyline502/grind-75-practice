320cd29e8bfe12dac4e5989367a85382b714dbf0
Skyline2011 <93230132+skyline502@users.noreply.github.com>
2022-12-22 17:35:59 -0800

    Update 08-diameter-of-bt.js
    Added solution not checked yet

--- week2/08-diameter-of-bt.js
+++ week2/08-diameter-of-bt.js
@@ -7,3 +7,47 @@ The diameter of a binary tree is the length of the longest path between any two 
 The length of a path between two nodes is represented by the number of edges between them.
 
 */
+
+
+class Node {
+  constructor(val){
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const btDiameter = root => {
+  if (!root) return 1;
+  let left = btDiameter(root.left)
+  let right = btDiameter(root.right)
+
+  return Math.max(left,right) + 1;
+}
+
+let a;
+let b;
+let c;
+let d;
+let e;
+
+a = new Node(5)
+b = new Node(4)
+c = new Node(3)
+d = new Node(2)
+e = new Node(1)
+
+c.left = d;
+c.right = b;
+d.left = e;
+b.right = a;
+
+/*
+      c
+     / \
+    d   b
+   /     \
+  e       a
+*/
+
+console.log(btDiameter(c)); //4
