5dd0f9bdc2b38e4b5b7303317b88fea371225faf
Johnny San <johnny.san@gmail.com>
2022-12-27 09:48:54 -0800

    added contains dupe

---
+++ week2/11-contains-dupe.js
@@ -0,0 +1,22 @@
+//Contains Duplicate
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+const containsDuplicate = nums => {
+
+}
+
+//Examples:
+
+let nums;
+
+nums = [1, 2, 3, 1]
+
+console.log(containsDuplicate(nums))//true [1, 1]
+
+nums = [1, 2, 3, 4]
+console.log(containsDuplicate(nums))//false
+
+nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+console.log(containsDuplicate(nums))//true
