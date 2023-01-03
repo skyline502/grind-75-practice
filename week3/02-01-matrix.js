5060c7e471160da6ccfa208ef2c730b952033b84
Skyline2011 <93230132+skyline502@users.noreply.github.com>
2023-01-03 15:10:01 -0800

    Update 02-01-matrix.js
    Added solution I understand

--- week3/02-01-matrix.js
+++ week3/02-01-matrix.js
@@ -30,6 +30,48 @@ There is at least one 0 in mat.
 
 */
 
-const findZeros = (matrix, r, c) => {
+var updateMatrix = function (mat) {
+    const m = mat.length;
+    const n = mat[0].length;
 
-}
+    // create m*n empty matrix
+    const result = Array(m).fill().map(() => Array(n).fill())
+
+  console.log(result, "result")
+    
+    //create a queue to hold all the index values that hold 0 
+    // push 0 into the "result" matrix if value is 0 for that index
+
+    const queue = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (mat[i][j] === 0) {
+                result[i][j] = 0
+                queue.push([i, j])
+            }
+        }
+    }
+
+  console.log(result, "result", "queue:", queue)
+
+    while (queue.length) {
+       const [x, y] = queue.shift();
+    // if condition satisfy , add new index in queue and update its value in result matrix
+        if (x - 1 >= 0 && result[x - 1][y] === undefined)
+        {queue.push([x - 1, y]); result[x - 1][y] = result[x][y] + 1} //check up
+       
+        if (x + 1 < m && result[x + 1][y] === undefined) 
+        {queue.push([x + 1, y]); result[x + 1][y] = result[x][y] + 1 }  // check down 
+        
+        if (y - 1 >= 0 && result[x][y - 1] === undefined) 
+        { queue.push([x, y - 1]); result[x][y - 1] = result[x][y] + 1 } // check left
+       
+        if (y + 1 < n && result[x][y + 1] === undefined)
+        { queue.push([x, y + 1]); result[x][y + 1] = result[x][y] + 1 }  // check right
+    }
+    return result;
+};
+
+let mat = [[0,0,0],[0,1,0],[1,1,1]]
+
+console.log(updateMatrix(mat))
