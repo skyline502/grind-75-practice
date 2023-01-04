b7ae0fc8cdfb779bfa3f6c71f6dfc72cb401d817
skyline502 <johnny.san@gmail.com>
2023-01-03 21:20:30 -0800

    added example run down to understand 01 matrix more

--- week3/02-01-matrix.js
+++ week3/02-01-matrix.js
@@ -16,7 +16,7 @@ Output: [[0,0,0],[0,1,0],[0,0,0]]
 
 Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
 Output: [[0,0,0],[0,1,0],[1,2,1]]
- 
+
 
 Constraints:
 
@@ -30,49 +30,84 @@ There is at least one 0 in mat.
 
 */
 
-var updateMatrix = function (mat) {
-    const m = mat.length;
-    const n = mat[0].length;
+const updateMatrix = (mat) => {
+//   let mat = [
+//     [0, 0, 0],
+//     [0, 1, 0],
+//     [1, 1, 1],
+//   ];
+  const m = mat.length;
+  const n = mat[0].length;
 
-    // create m*n empty matrix
-    const result = Array(m).fill().map(() => Array(n).fill())
+  // create m*n empty matrix
+  const result = Array(m)
+    .fill()
+    .map(() => Array(n).fill());
+  /*
+    [
+        [ undefined, undefined, undefined ],
+        [ undefined, undefined, undefined ],
+        [ undefined, undefined, undefined ]
+    ]
+  */
 
-  console.log(result, "result")
-    
-    //create a queue to hold all the index values that hold 0 
-    // push 0 into the "result" matrix if value is 0 for that index
+  console.log(result, "result");
 
-    const queue = [];
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (mat[i][j] === 0) {
-                result[i][j] = 0
-                queue.push([i, j])
-            }
-        }
+  //create a queue to hold all the index values that hold 0
+  // push 0 into the "result" matrix if value is 0 for that index
+
+  const queue = [];
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (mat[i][j] === 0) {
+        result[i][j] = 0;
+        queue.push([i, j]);
+      }
     }
+  }
 
-  console.log(result, "result", "queue:", queue)
+  console.log(result, "result", "queue:", queue);
+  //   result:
+  //   [
+  //     [ 0, 0, 0 ],
+  //     [ 0, undefined, 0 ],
+  //     [ undefined, undefined, undefined ]
+  //   ]
 
-    while (queue.length) {
-//find undefined values by using known 0 and incrementing based on distance
-       const [x, y] = queue.shift();
+  //   queue:
+  //   [ [ 0, 0 ], [ 0, 1 ], [ 0, 2 ], [ 1, 0 ], [ 1, 2 ] ]
+
+  while (queue.length) {
+    //find undefined values by using 0's we found and indexed in our queue by searching around each 0 and incrementing based on distance
+    const [x, y] = queue.shift();
     // if condition satisfy , add new index in queue and update its value in result matrix
-        if (x - 1 >= 0 && result[x - 1][y] === undefined)
-        {queue.push([x - 1, y]); result[x - 1][y] = result[x][y] + 1} //check up
-       
-        if (x + 1 < m && result[x + 1][y] === undefined) 
-        {queue.push([x + 1, y]); result[x + 1][y] = result[x][y] + 1 }  // check down 
-        
-        if (y - 1 >= 0 && result[x][y - 1] === undefined) 
-        { queue.push([x, y - 1]); result[x][y - 1] = result[x][y] + 1 } // check left
-       
-        if (y + 1 < n && result[x][y + 1] === undefined)
-        { queue.push([x, y + 1]); result[x][y + 1] = result[x][y] + 1 }  // check right
-    }
-    return result;
+    if (x - 1 >= 0 && result[x - 1][y] === undefined) {
+      queue.push([x - 1, y]);
+      result[x - 1][y] = result[x][y] + 1;
+    } //check up
+
+    if (x + 1 < m && result[x + 1][y] === undefined) {
+      queue.push([x + 1, y]);
+      result[x + 1][y] = result[x][y] + 1;
+    } // check down
+
+    if (y - 1 >= 0 && result[x][y - 1] === undefined) {
+      queue.push([x, y - 1]);
+      result[x][y - 1] = result[x][y] + 1;
+    } // check left
+
+    if (y + 1 < n && result[x][y + 1] === undefined) {
+      queue.push([x, y + 1]);
+      result[x][y + 1] = result[x][y] + 1;
+    } // check right
+  }
+  return result;
 };
 
-let mat = [[0,0,0],[0,1,0],[1,1,1]]
+let mat = [
+  [0, 0, 0],
+  [0, 1, 0],
+  [1, 1, 1],
+];
 
-console.log(updateMatrix(mat))
+console.log(updateMatrix(mat));
