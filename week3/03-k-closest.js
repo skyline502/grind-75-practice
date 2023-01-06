b20bd27ae84a39d7e52c215fb456ea1c5b45e901
Skyline2011 <93230132+skyline502@users.noreply.github.com>
2023-01-05 19:18:46 -0800

    Update 03-k-closest.js
    Added my solution not verified yet

--- week3/03-k-closest.js
+++ week3/03-k-closest.js
@@ -5,3 +5,42 @@ The distance between two points on the X-Y plane is the Euclidean distance (i.e.
 
 You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
 */
+
+const closestKPoints = (arr, k) => {
+  let distances = []
+  let points = {}
+  let results = []
+
+  for (let point of arr) {
+    let length = Math.sqrt(Math.pow(point[0],2) + Math.pow(point[1], 2)).toFixed(2);
+    distances.push(length);
+
+    if(!points[length]) {
+      points[length] = [point]
+    } else {
+      points[length].push(point)
+    }
+  }
+  distances.sort()
+  for (let i = 0; i < k; i++) {
+    let p = points[distances[i]];
+    if (p.length === 1) {
+      results.push(p[0]);
+    } else if ((i + 1) < k) {
+      results.push(p[0])
+      results.push(p[1])
+    }
+    console.log(p[0])
+  }
+  return results;
+}
+
+let input = [[1,3], [-2,2]]
+let k = 1
+
+console.log(closestKPoints(input,k))
+
+input = [[3,3],[5,-1],[-2,4]]
+k = 2
+
+console.log(closestKPoints(input,k))
