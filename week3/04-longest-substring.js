ed7fc7c96672c0af7c21082393e06b5d697f12f7
Skyline2011 <93230132+skyline502@users.noreply.github.com>
2023-01-09 15:11:19 -0800

    Update 04-longest-substring.js
    Came up with solution that passes example test cases

--- week3/04-longest-substring.js
+++ week3/04-longest-substring.js
@@ -13,3 +13,39 @@ Have a current s
 Use loop 
 */
 
+const longestSub = s => {
+  let longest = ""
+  let current = ""
+  let i = 0
+
+  while (i < s.length) {
+    if (!current.includes(s[i])) {
+      current += s[i]
+    } else {
+      if (longest.length < current.length) {
+        longest = current
+        current = s[i]
+      } 
+    }
+    i++
+  }
+
+  if (current.length > longest.length){
+    longest =  current
+  }
+
+  console.log(longest)
+
+  return longest.length;
+}
+
+let s = "abcabcbb"
+
+console.log(longestSub(s))//3
+
+s=  "bbbbb"
+console.log(longestSub(s))//1
+
+s = "pwwkwe"
+
+console.log(longestSub(s))//3
