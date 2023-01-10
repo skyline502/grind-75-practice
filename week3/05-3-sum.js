6bea37f2704e7ae99402079da863ca3f390b4d7d
Skyline2011 <93230132+skyline502@users.noreply.github.com>
2023-01-09 19:01:23 -0800

    Update 05-3-sum.js
    Added unverified solution

--- week3/05-3-sum.js
+++ week3/05-3-sum.js
@@ -32,3 +32,53 @@ Constraints:
 -105 <= nums[i] <= 105
 
 */
+
+const threeSum = nums => {
+  nums.sort((a, b) => a - b);
+
+  console.log(nums)
+
+  let result = []
+
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] > 0){
+      //console.log(nums[i])
+      //console.log(nums, "dis")
+      break
+    }
+
+    if (i > 0 && nums[i] 
+        === nums[i - 1]) {
+      continue
+    }
+
+    let left = i + 1
+    let right = nums.length - 1
+
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right]
+
+
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]])
+
+        left++
+        right--
+
+        while(left < right && nums[left] === nums[left - 1]) {
+          left++
+        }
+      } else if (sum < 0) {
+        left++
+      } else {
+        right--
+      }
+    }
+  }
+  return result
+}
+
+
+let nums = [-1,0,1,2,-1,-4]
+
+console.log(threeSum(nums))
