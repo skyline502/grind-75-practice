cfd2e992f40ed44f875f39402f41e8d25e78d4d5
Skyline2011 <93230132+skyline502@users.noreply.github.com>
2023-01-10 17:39:01 -0800

    Update 06-BT-level-order.js
    Added my solution, non verified yet

--- week3/06-BT-level-order.js
+++ week3/06-BT-level-order.js
@@ -12,3 +12,52 @@ Example:
 Output: [[3],[9,20],[15,7]]
 
 */
+
+const levelOrder = root => {
+  if(!root) return [];
+
+  const result = [[root.val]];
+  const queue = [root];
+
+  while (queue.length) {
+    let current = queue.shift();
+    let level = [];
+
+    if (current.left) {
+      queue.push(current.left);
+      level.push(current.left.val);
+    }
+
+    if (current.right) {
+      queue.push(current.right);
+      level.push(current.right.val);
+    }
+
+    if (level.length > 0) {
+      result.push(level);
+    }
+  }
+
+  return result;
+}
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+let a = new Node(3)
+let b = new Node(9)
+let c = new Node(20)
+let d = new Node(15)
+let e = new Node(7)
+
+a.left = b
+a.right = c
+c.left = d 
+c.right = e
+
+console.log(levelOrder(a));
