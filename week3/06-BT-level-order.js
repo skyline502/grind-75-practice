cbedb55758b9969cc8ee8000ea3c4c1974c036fb
Skyline2011 <93230132+skyline502@users.noreply.github.com>
2023-01-10 17:11:03 -0800

    Create 06-BT-level-order.js

---
+++ week3/06-BT-level-order.js
@@ -0,0 +1,14 @@
+/*
+Given the root of a BT, return the level order
+Traversal of its node values.
+
+Example:
+
+      3
+     / \
+    9   20
+        / \
+       15  7
+Output: [[3],[9,20],[15,7]]
+
+*/
