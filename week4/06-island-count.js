8e07a93737e18a4dff87c33aa77b1e359421e55a
Skyline2011 <93230132+skyline502@users.noreply.github.com>
2023-01-25 17:36:13 -0800

    Update 06-island-count.js
    Changed

--- week4/06-island-count.js
+++ week4/06-island-count.js
@@ -34,3 +34,34 @@ n == grid[i].length
 grid[i][j] is '0' or '1'.
 
 */
+
+const islandCount = (grid) => {
+    let count = 0; //increment count for every full explored island
+    let visited = new Set(); //so we do not explore visited lands
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            if (explore(grid, r, c, visited)) count++;
+        }
+    }
+
+    return count;
+};
+
+const explore = (grid, r, c, visited) => {
+    const rowInbounds = 0 <= r && r < grid.length; //row cannot be less than 0, or greater than length of grid
+    const colInbounds = 0 <= c && c < grid[0].length; //col cannot be less than 0 or greater than length of row
+    if (!rowInbounds || !colInbounds) return false;
+
+    if (grid[r][c] === '0') return false;
+
+    const pos = r + ',' + c;
+    if (visited.has(pos)) return false;
+    visited.add(pos);
+
+    explore(grid, r - 1, c, visited);
+    explore(grid, r + 1, c, visited);
+    explore(grid, r, c + 1, visited);
+    explore(grid, r, c - 1, visited);
+
+    return true;
+}
